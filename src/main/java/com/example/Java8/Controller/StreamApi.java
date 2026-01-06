ee3b1596c18053975589747be468d01e410c060e
Gulshan Sharma <164346797+gulshan-notify@users.noreply.github.com>
2026-01-06 17:55:44 +0530

    Initial commit

---
+++ src/main/java/com/example/Java8/Controller/StreamApi.java
@@ -0,0 +1,13 @@
+package com.example.Java8.Controller;
+
+public class StreamApi {
+
+     public void practice(){
+         // System.out.println("hbvsxhj");
+     }
+
+     public static void main(String[] args) {
+          System.out.println("Hyy , What's Up!");
+     }
+
+}
